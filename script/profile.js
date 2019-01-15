9b8d0ca9782d12cfe932def4f533cd8acb94cea5
murat emir cabaroğlu <muratemir.19988@gmail.com>
2019-01-15 23:28:11 +0300

    profile page updated

--- script/profile.js
+++ script/profile.js
@@ -1,14 +1,11 @@
-$(document).ready(function() {
-    $(".btn-pref .btn").click(function () {
-        $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
-        // $(".tab").addClass("active"); // instead of this do the below 
-        $(this).removeClass("btn-default").addClass("btn-primary");   
-    });
-    });
+// $(".btn-pref .btn").click(function () {
+//     $(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
+//     // $(".tab").addClass("active"); // instead of this do the below 
+//     $(this).removeClass("btn-default").addClass("btn-primary");   
+// });
+$(document).ready(function(){
 
-
-    function ss(endpoint){
-        a="https://cors-anywhere.herokuapp.com/http://104.248.33.42:5000" + endpoint;
+    a="https://cors-anywhere.herokuapp.com/http://104.248.33.42:5000" + "/api/users/"+"mrtmr";
         
         $.ajax({
             type: 'GET',
@@ -16,71 +13,25 @@ $(document).ready(function() {
             //data:DATA,
             //dataType:JSON
         }).done(function(data) {
-            for(const trip of data) {
-                console.log(trip)
-                $( "#CardHolder" ).append(
-                    `<div class="card">
-                        <div class="card profile-card-5">
-                            <div class="card-img-block">
-                                <img class="card-img-top" style="height:100px; weight:100px;" src="https://marklangphotography.files.wordpress.com/2011/11/img_4003-v3-bw.jpg"
-                                    alt="Card image cap">
-                            </div>
-                            <div class="card-body" style="background-color:black;">
-                                <h5 class="card-title" style="color:antiquewhite;">From: ${trip.from} - To: ${trip.to}</h5>
-                                <p class="card-text" style="color:antiquewhite;">Date: ${trip.time} with ${trip.vehicle}</p>
-                
-                                <p align="right"> <a href="profile.html" class="btn btn-dark ">View</a></p>
-                            </div>
-                        </div>
+           
+                console.log(data)
+                $( "#ProfileData" ).append(
+                    `<h1>${data[0].name} ${data[0].surname}</h1>
+                        <p>${data[0].age} , ${data[0].nationality}</p>
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-light btn-lg"><a href="profileedit.html">Profil Düzenle</a></button>
                     </div>`
                     );
-            }
-            // data.forEach(element => {
-            // }); 
-    
-     
+
+                    $( "#BioData" ).append(
+                        `<h1 style="font-size:25px;">BIOGRAPHY</h1>
+                        <p>${data[0].bio}</p>
+                        `
+                        );
+
+
+            
         });
-    }
-    
-    function gg(endpoint){
-        a="https://cors-anywhere.herokuapp.com/http://104.248.33.42:5000" + endpoint;
-        
-        $.ajax({
-            type: 'POST',
-            url: a,
-            crossDomain: true,
-            data:JSON.stringify(DATA),
-            dataType:"json",
-            header: {
-    
-            }
-        }).done(function(data) {
-    
-    
-            console.log(data)
-            // for(const trip of data) {
-            //     console.log(trip)
-            //     $( "#CardHolder" ).append(
-            //         `<div class="card">
-            //             <div class="card profile-card-5">
-            //                 <div class="card-img-block">
-            //                     <img class="card-img-top" style="height:100px; weight:100px;" src="https://marklangphotography.files.wordpress.com/2011/11/img_4003-v3-bw.jpg"
-            //                         alt="Card image cap">
-            //                 </div>
-            //                 <div class="card-body" style="background-color:black;">
-            //                     <h5 class="card-title" style="color:antiquewhite;">From: ${trip.from} - To: ${trip.to}</h5>
-            //                     <p class="card-text" style="color:antiquewhite;">Date: ${trip.time} with ${trip.vehicle}</p>
-                
-            //                     <p align="right"> <a href="profile.html" class="btn btn-dark ">View</a></p>
-            //                 </div>
-            //             </div>
-            //         </div>`
-            //         );
-            // }
-            // // data.forEach(element => {
-            // // }); 
-    
-     
-        });
-    }
+});
+
 
